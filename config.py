8c8b84a9e48564374294bb09870d3e61dec9fa39
Vlad <saifulinvladislav@gmail.com>
2023-08-26 05:05:53 +0500

    changes 2

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TRAIN_EX = 333290*2
 TEST_EX = 333290*2
-EPOCHS = 20
+EPOCHS = 1
 TEST_PATH = "additional_data/building_20230808.csv"
--- dataset.py
+++ dataset.py
@@ -11,24 +11,25 @@ from tqdm import tqdm
 
 def load_dataset(path):
     df = pd.read_csv(path, sep=",", header=0)
-    return df[["target_building_id", "target_address"]][:10000]
+    return df[["target_building_id", "target_address"]]
 
 
 def create_pair_data(text, labels, size):
     data_dict = {}
-    for x in tqdm(pd.DataFrame({"text": text, "label": labels}).groupby("label").apply(
-            lambda x: list(x["text"])).reset_index().values.tolist()):
+    for x in pd.DataFrame({"text": text, "label": labels}).groupby("label").apply(
+            lambda x: list(x["text"])).reset_index().values.tolist():
         data_dict[x[0]] = x[1]
     label1 = []
     for d in data_dict.keys():
         label1.extend(list(itertools.combinations(data_dict[d], 2)))
 
     label0 = []
-    for d1 in data_dict.keys():
+    for d1 in tqdm(data_dict.keys()):
         for d2 in data_dict.keys():
+            if len(label0) >= len(label1):
+                break
             if d1 != d2:
                 label0.extend(list(itertools.product(data_dict[d1], data_dict[d2])))
-    print(len(label1), len(label0))
     label0 = random.sample(label0, len(label1))
     return random.sample(label1, int(size / 2)), random.sample(label0, int(size / 2))
 
